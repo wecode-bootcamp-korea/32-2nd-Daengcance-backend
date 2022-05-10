62c9befe919959568f38ee72011eee73201f0c94
jisoo choi <jisoochoi@jisooui-MacBookPro.local>
2022-05-10 22:33:29 +0900

    Create: models

---
+++ bookings/models.py
@@ -0,0 +1,15 @@
+import uuid
+
+from django.db   import models
+
+from core.models import TimeStampModel
+
+class Booking(TimeStampModel):
+    booking_code  = models.UUIDField(default=uuid.uuid4, editable=False)
+    checkin_date  = models.DateField()
+    checkout_date = models.DateField()
+    user_id       = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    petsitter_id  = models.ForeignKey('petsitters.Petsitter', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'bookings'
