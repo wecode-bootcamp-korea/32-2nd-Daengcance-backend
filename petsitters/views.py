1b15738c9c716247cc6ef5b79e0948c6fc8c1a67
Jinsup Yoon <jinsupyoon@Jinsupui-MacBookPro.local>
2022-05-17 14:18:19 +0900

    Modify: PetsitterView & PetsitterDetailView

--- petsitters/views.py
+++ petsitters/views.py
@@ -1,7 +1,7 @@
 from django.http                  import JsonResponse
 from django.db.models             import Q, Count
 from django.views                 import View
-from petsitters.models            import Petsitter, Type
+from petsitters.models            import Petsitter, Type, PetsitterImage
 from bookings.models              import Booking 
 
 class PetsitterListView(View):
@@ -11,16 +11,16 @@ class PetsitterListView(View):
         check_out    = request.GET.get('check_out', "2022-12-31")
         keyword      = request.GET.get('keyword', "")
 
-        pick_up      = request.GET.get('pick_up', None)
-        another_pet  = request.GET.get('another_pet', None)
-        yard         = request.GET.get('yard', None)
+        type_id      = request.GET.get('type_id', None)
 
         offset       = int(request.GET.get('offset', 0))
         limit        = int(request.GET.get('limit', 10))
 
         sorting_options = {
-            "low_price" : "price"
+            "low_price"      : "price", 
+            "many_comments"  : "-comment_count"        
         }
+
 
         # Booking_period  = Booking.objecs.filter(
         #     check_in_gte  = check_in,
@@ -30,42 +30,33 @@ class PetsitterListView(View):
 
         q = Q()
 
-        if another_pet:
-            q &= Q(another_pet = another_pet)
-        
-        if pick_up:
-            q &= Q(pick_up = pick_up)
-
-        if yard:
-            q &= Q(yard = yard)
+        if type_id :
+            q &= Q(type__in = type_id)
 
         if keyword:
-            q &= Q(pettisitter_address__icontains = keyword)
+            q &= Q(address__icontains = keyword)
         
         # q &= Q(id__in = available_petsitter_list)
         
-        petsitters = Petsitter.objects.annotate(comment_count = Count('comment__id')).all()\
-                                                    .filter(q).order_by(sorting_options.get(sort_by, "price"))[offset : offset+limit]
+        petsitters = Petsitter.objects.filter(q)\
+                                            .order_by(sorting_options.get(sort_by, "price")).annotate(comment_count = Count('comment__id')).all()[offset : offset+limit]
 
         results = [{           
             "id"               : petsitter.id, 
             "titile"           : petsitter.title,
             "price"            : int(petsitter.price),
             "grade"            : petsitter.grade,
-            "type"             : [{
-                "type_id"      : petsitter.type.id,
-                "type_name"    : petsitter.type.name,
-                } for petsitter_type in petsitters.types.all()],
+            "type"             : [type.name for type in petsitter.type.all()],
             "information"      : petsitter.information,
             "address"          : petsitter.address,
-            "petsitter_image"  : [image.petsitter_image_url for image in petsitters.petsitter_images.all()]
+            "petsitter_image"  : [petsitterimage.petsitter_image_url for petsitterimage in petsitter.petsitterimage_set.all()]
         } for petsitter in petsitters]
         return JsonResponse({"result" : results}, status=200)
 
 class PetsitterDetailView(View):
-    def get(self, request,petsitter_id):
+    def get(self,petsitter_id):
         try:
-            petsitter = Petsitter.objects.get(id__in=petsitter_id)
+            petsitter = Petsitter.objects.get(id=petsitter_id)
 
             result    = {
                 "id"               : petsitter.id, 
@@ -74,15 +65,12 @@ class PetsitterDetailView(View):
                 "price"            : int(petsitter.price),
                 "grade"            : petsitter.grade,
                 "count"            : petsitter.count,
-                "type"             : [{
-                    "type_id"      : petsitter.type.id,
-                    "type_name"    : petsitter.type.name,
-                    } for petsitter_type in petsitter.type.all()],
+                "type"             : [type.name for type in petsitter.type.all()],
                 "information"      : petsitter.information,
                 "address"          : petsitter.address,
                 "longitude"        : float(petsitter.longitude),
                 "latitude"         : float(petsitter.latitude),
-                "petsitter_image"  : [image.petsitter_image_url for image in petsitter.petsitter_images.all()]
+                "petsitter_image"  : [petsitterimage.petsitter_image_url for petsitterimage in petsitter.petsitterimage_set.all()]
             }
             return JsonResponse({"message" : result}, status=200)
 
